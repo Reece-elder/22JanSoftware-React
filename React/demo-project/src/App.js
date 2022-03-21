b44f7fbae131779bf7c88b2ecd4bc187efa18fe3
reece-elder <reece.elder@qa.com>
2022-03-21 11:56:59 +0000

    setting up project

---
+++ React/demo-project/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+// Within React, the data that is rendered is what each component (app.js) returns
+
+function App() {
+  return (
+    <div className="App">
+      <h1> This is a new header </h1>
+      <h3> This is a smaller subtitle </h3>
+      <p> My new text into this </p>
+    </div>
+  );
+}
+
+export default App;
